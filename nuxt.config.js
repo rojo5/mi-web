fceda6c02852ad79692506ac72522308a9500aae
Luis Lorenzo Cuadrado <luislor@MacBook-Pro-de-Luis.local>
2021-05-03 01:33:49 +0200

    Configuración de despliegue

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,10 +1,10 @@
 export default {
   // Target: https://go.nuxtjs.dev/config-target
-  target: 'server',
+  target: 'static',
 
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
-    title: 'Web Luis Lorenzo',
+    title: 'Luis Lorenzo | Web',
     htmlAttrs: {
       lang: 'es'
     },
