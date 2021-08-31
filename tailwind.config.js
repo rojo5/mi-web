91117823ae89f8e7d863bb421b8e01d702f72717
rojo5 <luis21197@gmail.com>
2021-08-31 20:02:11 +0200

    Corregido error de build y eleminada dependencia postcss

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,13 +1,5 @@
 module.exports = {
-  purge: [{
-    content: [
-      `components/**/*.{vue,js}`,
-      `layouts/**/*.vue`,
-      `pages/**/*.vue`,
-      `plugins/**/*.{js,ts}`,
-      `nuxt.config.{js,ts}`
-    ]
-  }],
+  purge: [],
   darkMode: 'class', // or 'media' or 'class'
   theme: {
     extend: {
